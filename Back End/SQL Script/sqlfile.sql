adebd95186156c8e47135048cbf5b446c27c005f
William Cesar <168695261+will-csc@users.noreply.github.com>
2025-05-23 20:41:39 -0300

    Update sqlfile.sql

--- Back End/SQL Script/sqlfile.sql
+++ Back End/SQL Script/sqlfile.sql
@@ -5,11 +5,10 @@ USE todo_app;
 /*------------------ TABELA DE USUARIOS ----------------*/
 CREATE TABLE IF NOT EXISTS users(
 	id_user INT PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR(50) NOT NULL,
-    e_mail VARCHAR(50) NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+    e_mail VARCHAR(50) NOT NULL UNIQUE,
     phone BIGINT NOT NULL,
     userpassword VARCHAR(50) NOT NULL,
-    image VARCHAR(1000),
     retrieve_code BIGINT NOT NULL,
     user_rank INT DEFAULT 1 NOT NULL
 );
@@ -21,7 +20,43 @@ CREATE TABLE IF NOT EXISTS documents(
 	id_docs INT PRIMARY KEY AUTO_INCREMENT,
     id_user INT NOT NULL,
     doc_name VARCHAR(100) NOT NULL,
-    deadline DATE,
+    doc_path VARCHAR(500) NOT NULL UNIQUE,
+    deadline DATE NOT NULL,
+    week_day VARCHAR(10) NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE
     CASCADE ON UPDATE CASCADE
 );
+
+/*---------------- FUNÇÃO PARA PREENCHER NOME DO DIA --------------------------*/
+DELIMITER $$
+
+CREATE FUNCTION nome_dia_semana(data DATE) RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+    DECLARE dia_semana INT;
+    SET dia_semana = WEEKDAY(data); -- Segunda = 0, Domingo = 6
+
+    RETURN CASE dia_semana
+        WHEN 0 THEN 'Segunda'
+        WHEN 1 THEN 'Terça'
+        WHEN 2 THEN 'Quarta'
+        WHEN 3 THEN 'Quinta'
+        WHEN 4 THEN 'Sexta'
+        WHEN 5 THEN 'Sábado'
+        WHEN 6 THEN 'Domingo'
+    END;
+END$$
+
+DELIMITER ;
+
+/*------------ TRIGGER PARA INSERÇÃO AUTOMÁTICA -----------------*/
+DELIMITER $$
+
+CREATE TRIGGER before_insert_documents
+BEFORE INSERT ON documents
+FOR EACH ROW
+BEGIN
+    SET NEW.week_day = nome_dia_semana(NEW.deadline);
+END$$
+
+DELIMITER ;
